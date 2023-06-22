1695e09437ee467557908a227f7a0402e62b1c7f
Bastifuri <128314591+Bastifuri@users.noreply.github.com>
2023-06-22 09:05:56 -0400

    Update Inmuebles.java

--- src/main/java/Model/Inmuebles.java
+++ src/main/java/Model/Inmuebles.java
@@ -1,4 +1,45 @@
 package Model;
 
-public class Inmueble {
+public class Inmuebles {
+  private String tipoC;
+    private String ciudad;
+    private String direccion;
+    private int precio;
+    public Inmueble(String tipoC,String ciudad,String direccion, int precio) {
+        this.tipoC= tipoC;
+        this.ciudad= ciudad;
+        this.direccion = direccion;
+        this.precio = precio;
+    }
+    public String getTipoC() {
+        return tipoC;
+    }
+
+    public void setTipoC(String tipoC) {
+        this.tipoC = tipoC;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
 }
